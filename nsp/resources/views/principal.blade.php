f12f52dc8bcf8283c20dd8e2643bd4494619568a
Carlos Bruno <cbcarlos08@gmail.com>
2017-10-20 12:11:05 -0400

    Sistema finalizado v1.0

--- nsp/resources/views/principal.blade.php
+++ nsp/resources/views/principal.blade.php
@@ -11,7 +11,7 @@
   <nav class="navbar navbar-default navbar-inverse navbar-fixed-top">
       <div class="container-fluid">
           <div class="navbar-header">
-              <a class="navbar-brand" href="{{ action('NotificacaoController@principal') }}">Notifica&ccedil;&otilde;es<b>NSP</b></a>
+              <a class="navbar-brand" href="{{ action('NotificacaoController@principal') }}">Notifica&ccedil;&otilde;es<b>NSP</b>v1.0</a>
           </div>
           <ul class="nav navbar-nav">
               <li><a href="{{ action('NotificacaoController@cadastro') }}">Cadastro</a></li>
